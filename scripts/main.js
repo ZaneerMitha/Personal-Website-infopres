fa276659123613ab5307406441c2148275783c7a
Zaneer Mitha <100327436+ZaneerMitha@users.noreply.github.com>
2024-01-24 21:18:43 -0500

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -7,37 +7,35 @@ const myImage = document.querySelector("img");
 
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
+  if (mySrc === "images/myPic2.jpg") {
     myImage.setAttribute("src", "images/ZMLogo.png");
-  } else {
-    myImage.setAttribute("src", "images/ZMLogoWhite.png");
-  }
+  } 
 };
 
-document.querySelector("html").addEventListener("click", () => {
-    alert("Ouch! Stop poking me!");
+document.querySelector("button").addEventListener("click", () => {
+    alert("One day your life will be a book, you better make it worth reading.");
   });
 
 let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
+// let myHeading = document.querySelector("h1");
 
-function setUserName() {
-    const myName = prompt("Please enter your name.");
-    if (!myName) {
-      setUserName();
-    } else {
-      localStorage.setItem("name", myName);
-      myHeading.textContent = `Zaneer is cool, ${myName}`;
-    }
-  }
+// function setUserName() {
+//     const myName = prompt("Please enter your name.");
+//     if (!myName) {
+//       setUserName();
+//     } else {
+//       localStorage.setItem("name", myName);
+//       myHeading.textContent = `Zaneer is cool, ${myName}`;
+//     }
+//   }
 
-  if (!localStorage.getItem("name")) {
-    setUserName();
-  } else {
-    const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Zaneer is cool, ${storedName}`;
-  }
+//   if (!localStorage.getItem("name")) {
+//     setUserName();
+//   } else {
+//     const storedName = localStorage.getItem("name");
+//     myHeading.textContent = `Zaneer is cool, ${storedName}`;
+//   }
 
-  myButton.onclick = () => {
-    setUserName();
-  };
+//   myButton.onclick = () => {
+//     setUserName();
+//   };
